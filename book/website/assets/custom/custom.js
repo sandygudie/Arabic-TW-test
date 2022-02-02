1ea0d2416c75d9d7ea30964a0c1a466062f6ad64
Batool Almarzouq <53487593+BatoolMM@users.noreply.github.com>
2022-02-02 21:19:36 +0300

    delete custom.js

--- book/website/assets/custom/custom.js
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript">
-    var _jipt = [];
-    _jipt.push(['project', '92b4e205e5c45c89928927d6101b4fbc']);
-    _jipt.push(['domain', 'turingway']);
-</script>
-<script type="text/javascript" src="//cdn.crowdin.com/jipt/jipt.js"></script>
