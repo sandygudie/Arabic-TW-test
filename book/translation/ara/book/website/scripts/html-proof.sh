8b0774d7cbadbdb17d7065ed30153a7850954238
Batool Almarzouq <53487593+BatoolMM@users.noreply.github.com>
2022-03-22 16:28:34 +0300

    New translations html-proof.sh (Arabic)

--- book/translation/ara/book/website/scripts/html-proof.sh
+++ book/translation/ara/book/website/scripts/html-proof.sh
@@ -1,10 +1,10 @@
-crwdns34111:0crwdne34111:0
-crwdns34113:0crwdne34113:0
+#!/usr/bin/env bash
+تعيين -e # إيقاف البرنامج النصي عند الخطأ
 
-crwdns34115:0crwdne34115:0 
-crwdns34117:0crwdne34117:0
-crwdns34119:0crwdne34119:0
-crwdns34121:0crwdne34121:0
-crwdns34123:0crwdne34123:0
-crwdns34125:0crwdne34125:0 
+إنشاء موقع 
+حزمة exec htmlproofer --allow-hash-href \
+--افترض-ملحق./_الموقع \
+--تجاهل url "/\/apple-touch*.*/,/\/images/logo/favicon.ico/,/#*/" \
+--ديناص-خارجي \
+--فقط_4xx 
 
